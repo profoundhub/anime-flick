c01453e15a64f08894c8ed4d11f1d2e5485218cb
khaleelsyed <khaleel@khaleelsyed.com>
2016-07-25 22:58:27 +0100

    Checkpoint

---
+++ catalogue.js
@@ -0,0 +1,26 @@
+$.material.init();
+//var url='';
+/*
+$(document).ready(function(){
+  $(#submit).click(function(){
+    url='http://www.omdbapi.com/?t='+$('#queryMovie').val();
+    $getJSON(function(data){
+    });
+  });
+});
+*/
+
+
+
+/*
+$(document).ready(function(){
+  $('#submit').click(function(){
+    //url='http://myanimelist.net/api/anime/search.xml?q='+$('#queryAnime').val();
+    var xml = 'http://myanimelist.net/api/anime/search.xml?q='+$('#queryAnime').val(),
+    xmlDoc = $.parseXML(xml),
+    $xml = $(xmlDoc),
+    $title = $xml.find("title");
+    $("#result").append( $title.text());
+  });
+});
+*/
