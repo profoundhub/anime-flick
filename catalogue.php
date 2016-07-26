8900f9ebd36b5f7fc6832f6532aff6d91a6f17b1
khaleelsyed <khaleel@khaleelsyed.com>
2016-07-26 19:43:41 +0100

    Allow 'basic' movie search
    Currently will only display a single movie at a time.

--- catalogue.php
+++ catalogue.php
@@ -1,20 +1,28 @@
 <?php include('includes/header.php'); ?>
 
 <h1 class="pager">Catalogue</h1>
-	<form class="form-group">
-		<input type='text' class="form-control is-empty" id='query' placeholder='Search'>
-		<button class="btn btn-primary" type="button" id="button">Search</button>
-		<div class="row">
-			<div class="col-xs-2">
-				<label><input type="radio" name="select" value="movie" checked=''> Movie</label>
-			</div>
-			<div class="col-xs-2">
-				<label><input type="radio" name="select" value="anime"> Anime</label>
-			</div>
+<form class="form-group">
+	<input type='text' class="form-control box-q" id='queryName' placeholder='Name'>
+	<input type="text" class="form-control is-empty box-q" id='queryYear' placeholder="Year">
+	<button class="btn btn-primary" type="button" id="button">Search</button>
+	<div class="row">
+		<div class="col-xs-2">
+			<label><input type="radio" name="select" value="movie" checked='' id="radioMovie"> Movie</label>
 		</div>
-
-	</form>
-<h3>Results: </h3>
-<p id='result'>Result will populate over here</p>
+		<div class="col-xs-2">
+			<label><input type="radio" name="select" value="series" id="radioSeries"> TV Series</label>
+		</div>
+	</div>
+</form>
+<div id='result'>
+	<h3>Results: </h3>
+	<div id='name'></div>
+	<div id='genre'></div>
+	<div id='year'></div>
+	<div id='releaseDate'></div>
+	<div id='runtime'></div>
+	<div id='rating'></div>
+	<div id='plot'></div>
+</div>
 
 <?php include('includes/footer.php'); ?>
--- main.js
+++ main.js
@@ -1,6 +1,34 @@
 $.material.init();
 $(document).ready(function(){
+	//console.log("Works");
 	$("#button").click(function(){
-		consle.log('Button click recived!');
+		if($("#radioMovie").prop("checked")){
+			url='http://www.omdbapi.com/?type=movie&plot=full&t='+$("#queryName").val();
+			if($(queryYear).val()===""){
+				$.getJSON(url,function(data){
+					$('#name').html("<h5>Movie: </h5><p>"+data.Title+"</p>");
+					$('#genre').html("<h5>Genre: </h5><p>"+data.Genre+"</p>");
+					$('#year').html("<h5>Year: </h5><p>"+data.Year+"</p>");
+					$('#releaseDate').html("<h5>Release Year: </h5><p>"+data.Released+"</p>");
+					$('#runtime').html("<h5>Runtime: </h5><p>"+data.Runtime+"</p>");
+					$('#rating').html("<h5>imDB Rating: </h5><p>"+data.imdbRating+"</p>");
+					$('#plot').html("<h5>imDB Rating: </h5><p>"+data.Plot+"</p>");
+				});
+			}else{
+				url='http://www.omdbapi.com/?type=movie&plot=full&t='+$("#queryName").val()+"&y="+$("#queryYear").val();
+				$.getJSON(url,function(data){
+					$('#name').html("<h5>Movie: </h5><p>"+data.Title+"</p>");
+					$('#genre').html("<h5>Genre: </h5><p>"+data.Genre+"</p>");
+					$('#year').html("<h5>Year: </h5><p>"+data.Year+"</p>");
+					$('#releaseDate').html("<h5>Release Year: </h5><p>"+data.Released+"</p>");
+					$('#runtime').html("<h5>Runtime: </h5><p>"+data.Runtime+"</p>");
+					$('#rating').html("<h5>imDB Rating: </h5><p>"+data.imdbRating+"</p>");
+					$('#plot').html("<h5>imDB Rating: </h5><p>"+data.Plot+"</p>");
+				});
+			}
+		}
+		if($("#radioSeries").prop("checked")){
+			alert("Search is not currently available for TV Series");
+		}
 	});
 });
